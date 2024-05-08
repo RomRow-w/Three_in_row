f0c0b13018ec76f3c94d1001a6900d513ca29459
ME <example.com>
2024-05-08 22:46:16 +0500

    feat: random item placement board

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import { useState,useEffect } from "react";
+import './App.css'
+
+const COLORS = ['Brown','Green','Purple','Red','White','Yellow']
 
 
 function App() {
+
+  const [board,setBoard] = useState<string[]>([]);
+
+  const createGrid = () => {
+    const colors:string[] = new Array(64) 
+      .fill('')
+      .map(() => COLORS[Math.floor(Math.random() * COLORS.length)])
+
+    return colors
+  }
+
+  useEffect(() => {
+    setBoard(createGrid)
+  },[])
+
+
   return (
     <div className="App">
-
+      <div className="container">    
+        {board.map((item,index) => {
+          return <div className={item}>{index}</div>
+        })}
+      </div>
     </div>
   );
 }
